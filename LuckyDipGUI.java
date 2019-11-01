41b24597be6c32457303ce4082b46f5ea9f278c2
James <james689@users.noreply.github.com>
2019-11-01 18:47:37 +0000

    add thunderball panel to window, and add euromillions and thunderball lucky dip ticket generators

--- LuckyDipGUI.java
+++ LuckyDipGUI.java
@@ -10,6 +10,12 @@ import javax.swing.BorderFactory;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.BoxLayout;
 
 public class LuckyDipGUI extends JPanel {
 	
@@ -25,46 +31,137 @@ public class LuckyDipGUI extends JPanel {
 	// --------------------------------------------------
 	
 	public LuckyDipGUI() {
-		JPanel euroMillionsPanel = new JPanel(new BorderLayout());
-		//euroMillionsPanel.setBackground(Color.BLUE);
-		
-		JLabel euroMillionsLabel = new JLabel("EuroMillions: 5 numbers from 1 to 50 and 2 Lucky Stars from 1 to 12");
-		euroMillionsLabel.setFont(new Font("Serif", Font.PLAIN, 20));
-		
-		JPanel labelsPanel = new JPanel();
-		//labelsPanel.setBackground(Color.RED);
-		
-		Border outerBorder = BorderFactory.createLineBorder(Color.BLACK, 2);
-		Border margin = new EmptyBorder(10,10,10,10);
-		Border compoundBorder = new CompoundBorder(outerBorder, margin);
-		
-		JLabel label1 = new JLabel("1");
-		label1.setBorder(compoundBorder);
-		JLabel label2 = new JLabel("59");
-		label2.setBorder(compoundBorder);
-		JLabel label3 = new JLabel("39");
-		label3.setBorder(compoundBorder);
-		JLabel label4 = new JLabel("50");
-		label4.setBorder(compoundBorder);
-		JLabel label5 = new JLabel("22");
-		label5.setBorder(compoundBorder);
-		JLabel label6 = new JLabel("1");
-		label6.setBorder(compoundBorder);
-		JLabel label7 = new JLabel("10");
-		label7.setBorder(compoundBorder);
-		labelsPanel.add(label1);
-		labelsPanel.add(label2);
-		labelsPanel.add(label3);
-		labelsPanel.add(label4);
-		labelsPanel.add(label5);
-		labelsPanel.add(label6);
-		labelsPanel.add(label7);
-		
-		euroMillionsPanel.add(euroMillionsLabel, BorderLayout.NORTH);
-		euroMillionsPanel.add(new JButton("lucky dip"), BorderLayout.WEST);
-		euroMillionsPanel.add(labelsPanel, BorderLayout.CENTER);
-		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); // stack components vertically on top of each other
+		JPanel euroMillionsPanel = new EuroMillionsLotteryPanel("EuroMillions: 5 numbers from 1 to 50 and 2 Lucky Stars from 1 to 12");
+		JPanel thunderBallPanel = new ThunderballLotteryPanel("Thunderball: 5 numbers from 1 to 39 and 1 Thunderball from 1 to 14");
 		add(euroMillionsPanel);
+		add(thunderBallPanel);
 	}
+	
+	private abstract class LotteryPanel extends JPanel implements ActionListener {
+		
+		private JLabel titleLabel;
+		private JButton luckyDipButton;
+		
+		public LotteryPanel(String title) {
+			setLayout(new BorderLayout());
+			//setBackground(Color.BLUE);
+			titleLabel = new JLabel(title);
+			titleLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+			luckyDipButton = new JButton("lucky dip");
+			luckyDipButton.addActionListener(this); // panel listens for its own events
+			add(titleLabel, BorderLayout.NORTH);
+			add(luckyDipButton, BorderLayout.WEST);
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("lottery panel button clicked");
+		}
+	}
+	
+	private class EuroMillionsLotteryPanel extends LotteryPanel {
+		
+		private JPanel labelsPanel;
+		private List<Integer> numbers;
+		private JLabel[] numberLabels;
+		
+		public EuroMillionsLotteryPanel(String title) {
+			super(title);
+			Border outerBorder = BorderFactory.createLineBorder(Color.BLACK, 2);
+			Border margin = new EmptyBorder(10,10,10,10);
+			Border compoundBorder = new CompoundBorder(outerBorder, margin); // https://stackoverflow.com/questions/22384414/how-can-i-set-the-margin-of-a-jlabel
+			numbers = generateEuroMillionsNumbers();
+			labelsPanel = new JPanel();
+			//labelsPanel.setBackground(Color.RED);
+			numberLabels = new JLabel[7];
+			for (int i = 0; i < numberLabels.length; i++) {
+				numberLabels[i] = new JLabel("" + numbers.get(i)); // random number between 1 and 50
+				numberLabels[i].setBorder(compoundBorder);
+				labelsPanel.add(numberLabels[i]);
+			}
+			add(labelsPanel, BorderLayout.CENTER);
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("euro millions panel button clicked");
+			numbers = generateEuroMillionsNumbers();
+			for (int i = 0; i < numberLabels.length; i++) {
+				numberLabels[i].setText("" + numbers.get(i));
+			}
+		}
+		
+		private List<Integer> generateEuroMillionsNumbers() {
+			List<Integer> mainNumbers = getRandomNumbers(5,1,50);
+			List<Integer> luckyStars = getRandomNumbers(2,1,12);
+			List<Integer> newList = new ArrayList<>();
+			newList.addAll(mainNumbers);
+			newList.addAll(luckyStars);
+			return newList;
+		}
+	}
+	
+	private class ThunderballLotteryPanel extends LotteryPanel {
+		
+		private JPanel labelsPanel;
+		private List<Integer> numbers;
+		private JLabel[] numberLabels;
+		
+		public ThunderballLotteryPanel(String title) {
+			super(title);
+			Border outerBorder = BorderFactory.createLineBorder(Color.BLACK, 2);
+			Border margin = new EmptyBorder(10,10,10,10);
+			Border compoundBorder = new CompoundBorder(outerBorder, margin); // https://stackoverflow.com/questions/22384414/how-can-i-set-the-margin-of-a-jlabel
+			numbers = generateThunderballNumbers();
+			labelsPanel = new JPanel();
+			//labelsPanel.setBackground(Color.RED);
+			numberLabels = new JLabel[6];
+			for (int i = 0; i < numberLabels.length; i++) {
+				numberLabels[i] = new JLabel("" + numbers.get(i)); 
+				numberLabels[i].setBorder(compoundBorder);
+				labelsPanel.add(numberLabels[i]);
+			}
+			add(labelsPanel, BorderLayout.CENTER);
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("thunderball panel button clicked");
+			numbers = generateThunderballNumbers();
+			for (int i = 0; i < numberLabels.length; i++) {
+				numberLabels[i].setText("" + numbers.get(i));
+			}
+		}
+		
+		private List<Integer> generateThunderballNumbers() {
+			List<Integer> mainNumbers = getRandomNumbers(5,1,39);
+			List<Integer> thunderBall = getRandomNumbers(1,1,14);
+			List<Integer> newList = new ArrayList<>();
+			newList.addAll(mainNumbers);
+			newList.addAll(thunderBall);
+			return newList;
+		}
+	}
+	
+	// returns a list containing listSize unique random numbers, each within
+	// the range minBound (inclusive) to maxBound (inclusive)
+	private List<Integer> getRandomNumbers(int listSize, int minBound, int maxBound) {
+		List<Integer> randomNumbersList = new ArrayList<>();
+		while (randomNumbersList.size() < listSize) {
+			int randomNum = mathRandom(minBound,maxBound+1);
+			if (!randomNumbersList.contains(randomNum)) {
+				randomNumbersList.add(randomNum);
+			}
+		}
+		return randomNumbersList;
+	}
+	
+	// returns a random number between min(inclusive) and max(exclusive)
+    // equivalent to lua math.random(min,max) except the max is also inclusive in lua
+    public static int mathRandom(int min, int max) {
+        Random r = new Random();
+        int result = r.nextInt(max - min) + min;
+        return result;
+    }
 	
 }
