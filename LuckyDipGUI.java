cd98deb2de49b3b82d4c72d6a0d338cf996af5cf
James <james689@users.noreply.github.com>
2019-11-02 19:04:09 +0000

    create thunderball custom border

--- LuckyDipGUI.java
+++ LuckyDipGUI.java
@@ -1,5 +1,7 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.Component;
+import java.awt.Graphics;
 import java.awt.Dimension;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -8,14 +10,19 @@ import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.BorderFactory;
 import javax.swing.border.Border;
+import javax.swing.border.AbstractBorder;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.BoxLayout;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.Graphics2D;
 
 public class LuckyDipGUI extends JPanel {
 	
@@ -109,16 +116,21 @@ public class LuckyDipGUI extends JPanel {
 		
 		public ThunderballLotteryPanel(String title) {
 			super(title);
-			Border outerBorder = BorderFactory.createLineBorder(Color.BLACK, 2);
-			Border margin = new EmptyBorder(10,10,10,10);
-			Border compoundBorder = new CompoundBorder(outerBorder, margin); // https://stackoverflow.com/questions/22384414/how-can-i-set-the-margin-of-a-jlabel
+			
 			numbers = generateThunderballNumbers();
+			
+			Border mainNumberOuterBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
+			Border thunderballBorder = new ThunderballBorder(15);
+			Border margin = new EmptyBorder(10,10,10,10);
+			Border mainNumberCompoundBorder = new CompoundBorder(mainNumberOuterBorder, margin); // https://stackoverflow.com/questions/22384414/how-can-i-set-the-margin-of-a-jlabel
+			//Border thunderballCompoundBorder = new CompoundBorder(thunderballBorder, margin);
+			
 			labelsPanel = new JPanel();
 			//labelsPanel.setBackground(Color.RED);
 			numberLabels = new JLabel[6];
 			for (int i = 0; i < numberLabels.length; i++) {
 				numberLabels[i] = new JLabel("" + numbers.get(i)); 
-				numberLabels[i].setBorder(compoundBorder);
+				numberLabels[i].setBorder((i != 5) ? mainNumberCompoundBorder : thunderballBorder);
 				labelsPanel.add(numberLabels[i]);
 			}
 			add(labelsPanel, BorderLayout.CENTER);
@@ -141,6 +153,81 @@ public class LuckyDipGUI extends JPanel {
 			newList.addAll(thunderBall);
 			return newList;
 		}
+	}
+	
+	private class ThunderballBorder extends AbstractBorder {
+		private int ballRadius = 15;
+		
+		public ThunderballBorder() {} // use default ball radius
+		public ThunderballBorder(int ballRadius) { this.ballRadius = ballRadius; }
+		
+		@Override
+		public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                          RenderingHints.VALUE_ANTIALIAS_ON);
+			Insets borderInsets = getBorderInsets(c);
+			
+			// highlights the border insets area available to draw in (helps with debugging)
+			// The last four parameters of this method can be used to calculate the total screen area of
+			// the component (including the border insets) (see pg 409 swing book)
+			/*
+			g.setColor(Color.BLUE);
+			g.fillRect(x,y,width,borderInsets.top); // top border inset
+			g.setColor(Color.GREEN);
+			g.fillRect(x,y,borderInsets.left,height); // left border inset
+			g.setColor(Color.RED);
+			g.fillRect(x+width-borderInsets.right,y,borderInsets.right,height); // right border inset
+			g.setColor(Color.BLACK);
+			g.fillRect(x,y+height-borderInsets.bottom,width,borderInsets.bottom); // bottom border inset
+			*/
+			
+			g.setColor(Color.BLACK);
+			// draw the outline of a rectangle within the BorderInsets region
+			// the width of the rectangle will be the width of the component minus
+			// half of the left border inset and half of the right border inset.
+			// similarly, the height of the rectangle will be the height of the component
+			// minus half of the top and bottom border insets.
+			int rectX = borderInsets.left/2;
+			int rectY = borderInsets.top/2;
+			int rectWidth = c.getWidth() - borderInsets.left/2 - borderInsets.right/2;
+			int rectHeight = c.getHeight() - borderInsets.top/2 - borderInsets.bottom/2;
+			g.drawRect(rectX,rectY,rectWidth,rectHeight);
+			
+			/* old implementation for drawing the border lines:
+			int topLineYPos = y+(borderInsets.top/2);
+			int rightLineXPos = width-(borderInsets.right/2);
+			int leftLineXPos = x+(borderInsets.left/2);
+			int bottomLineYPos = height-(borderInsets.bottom/2);
+			g.setColor(Color.WHITE);
+			g.drawLine(leftLineXPos,topLineYPos,width,topLineYPos); // draw top line
+			g.drawLine(rightLineXPos,y,rightLineXPos,bottomLineYPos); // draw right line
+			g.drawLine(leftLineXPos,topLineYPos,leftLineXPos,bottomLineYPos); // draw left line
+			g.drawLine(leftLineXPos,bottomLineYPos,rightLineXPos,bottomLineYPos); // draw bottom line
+			*/
+			
+			int thunderballX = width-ballRadius;
+			int thunderballY = 0;
+			g.setColor(Color.RED);
+			g.fillOval(thunderballX,thunderballY,ballRadius,ballRadius); // draw thunderball
+		}
+		
+		// to keep things simple all border insets have the same size as the ball radius,
+		// even though the left and bottom insets don't need to be ball radius in size.
+		public Insets getBorderInsets(Component c) {
+			int top = ballRadius;
+			int left = ballRadius;
+			int bottom = ballRadius;
+			int right = ballRadius;
+			return new Insets(top,left,bottom,right); 
+		}
+		
+		public Insets getBorderInsets(Component c, Insets i) {
+			i.top = i.left = i.bottom = i.right = ballRadius;
+			return i;
+		}
+		
+		public boolean isBorderOpaque() { return true; }
 	}
 	
 	// returns a list containing listSize unique random numbers, each within
